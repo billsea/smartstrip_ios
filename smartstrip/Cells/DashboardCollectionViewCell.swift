c9c4a7003b581b7db548ea3107659f782266209f
Bill S <bill.e.seaman@gmail.com>
2018-07-09 19:03:49 -0700

    Add collection view dashboard and navigation

---
+++ smartstrip/Cells/DashboardCollectionViewCell.swift
@@ -0,0 +1,20 @@
+//
+//  DashboardCollectionViewCell.swift
+//  smartstrip
+//
+//  Created by Loud on 7/9/18.
+//  Copyright © 2018 Loudsoftware. All rights reserved.
+//
+
+import UIKit
+
+class DashboardCollectionViewCell: UICollectionViewCell {
+
+	@IBOutlet weak var cellNameLabel: UILabel!
+	
+	override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
