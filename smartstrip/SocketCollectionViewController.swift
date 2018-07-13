4b196260a4493c355225e1cbe75387a71e6e2580
Bill S <bill.e.seaman@gmail.com>
2018-07-12 18:26:28 -0700

    Add socket select ui update from return message from arduino ble shield

--- smartstrip/SocketCollectionViewController.swift
+++ smartstrip/SocketCollectionViewController.swift
@@ -12,10 +12,15 @@ import CoreBluetooth
 private let reuseIdentifier = "Cell"
 private let bleShieldName = "HMSoft"
 
-struct view_socket {
+class view_socket {
 	var name : String?
 	var image : UIImage?
-	var selected : Bool?
+	var active : Bool?
+	init(name: String?, image : UIImage?, active : Bool?) {
+		self.name = name
+		self.image = image
+		self.active = active
+	}
 }
 
 //<CBPeripheral: 0x105427da0, identifier = 26F927A3-C31C-D452-AEBA-FB2D5FC71825, name = HMSoft, state = disconnected>
@@ -47,12 +52,12 @@ class SocketCollectionViewController: UICollectionViewController, CBCentralManag
 			centralManager = CBCentralManager(delegate: self, queue: nil)
 			
 			//data source
-			cv_items.append(view_socket(name: "One", image: UIImage(named: "help"), selected: true))
-			cv_items.append(view_socket(name: "Two", image: UIImage(named: "help"), selected: true))
-			cv_items.append(view_socket(name: "Three", image: UIImage(named: "help"), selected: false))
-			cv_items.append(view_socket(name: "Four", image: UIImage(named: "help"), selected: true))
-			cv_items.append(view_socket(name: "Five", image: UIImage(named: "help"), selected: true))
-			cv_items.append(view_socket(name: "Six", image: UIImage(named: "help"), selected: true))
+			cv_items.append(view_socket(name: "One", image: UIImage(named: "help"), active: true))
+			cv_items.append(view_socket(name: "Two", image: UIImage(named: "help"), active: true))
+			cv_items.append(view_socket(name: "Three", image: UIImage(named: "help"), active: true))
+			cv_items.append(view_socket(name: "Four", image: UIImage(named: "help"), active: true))
+			cv_items.append(view_socket(name: "Five", image: UIImage(named: "help"), active: true))
+			cv_items.append(view_socket(name: "Six", image: UIImage(named: "help"), active: true))
 			
 			// Register cell classes
 			self.collectionView!.register(UINib(nibName: "SocketCollectionViewCell", bundle: nil), forCellWithReuseIdentifier: reuseIdentifier)
@@ -80,122 +85,133 @@ class SocketCollectionViewController: UICollectionViewController, CBCentralManag
 		self.present(alertController, animated: false, completion: nil)
 	}
 	
+
+	// MARK: UICollectionViewDataSource
+	override func numberOfSections(in collectionView: UICollectionView) -> Int {
+			return 1
+	}
+
+	override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+			return cv_items.count
+	}
+
+	override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+			let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath) as! SocketCollectionViewCell
 	
+		// Configure the cell
+		let current_socket = cv_items[indexPath.row] as view_socket
+		cell.backgroundColor = current_socket.active! ? UIColor.green : UIColor.red
+		cell.cellName.text =  current_socket.name
+		cell.cellImage.image = current_socket.image
 	
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+			return cell
+	}
 
-    // MARK: UICollectionViewDataSource
-    override func numberOfSections(in collectionView: UICollectionView) -> Int {
-        return 1
-    }
-
-
-    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return cv_items.count
-    }
-
-    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-       	let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath) as! SocketCollectionViewCell
-    
-			// Configure the cell
-			let current_socket = cv_items[indexPath.row] as view_socket
-			cell.backgroundColor = current_socket.selected! ? UIColor.green : UIColor.red
-			cell.cellName.text =  current_socket.name
-			cell.cellImage.image = current_socket.image
-    
-        return cell
-    }
-
-    // MARK: UICollectionViewDelegate
-
-    /*
-    // Uncomment this method to specify if the specified item should be highlighted during tracking
-    override func collectionView(_ collectionView: UICollectionView, shouldHighlightItemAt indexPath: IndexPath) -> Bool {
-        return true
-    }
-    */
-
-
-    // Uncomment this method to specify if the specified item should be selected
-    override func collectionView(_ collectionView: UICollectionView, shouldSelectItemAt indexPath: IndexPath) -> Bool {
-			var sel_index = indexPath.row
-			if(sel_index < 6){
-				if(indexPath.row == 3){
-					sel_index = 2
-				} else if(indexPath.row >= 4){
-					sel_index = 3
-				}
-				let position_string = String(sel_index)
-				let position_string_data = position_string.data(using: String.Encoding.utf8)
-				
-				//TODO: CBCharacteristicWriteType.withResponse doesn't write data....need a way to get the
-				// on-off status from the bluetooth shield somehow
-				HmSoftPeripheral?.writeValue(position_string_data!, for: self.positionCharacteristic!, type: CBCharacteristicWriteType.withoutResponse)
-			}
+	// MARK: UICollectionViewDelegate
+	/*
+	// Uncomment this method to specify if the specified item should be highlighted during tracking
+	override func collectionView(_ collectionView: UICollectionView, shouldHighlightItemAt indexPath: IndexPath) -> Bool {
 			return true
-    }
-	
-	//MARK: BLE Calls
-	
-		// MARK: BLE Scanning
-		func scanBLEDevices() {
-			//manager?.scanForPeripherals(withServices: [CBUUID.init(string: parentView!.BLEService)], options: nil)
-			
-			//if you pass nil in the first parameter, then scanForPeriperals will look for any devices.
-			centralManager?.scanForPeripherals(withServices: nil, options: nil)
-			
-			//stop scanning after 3 seconds
-			DispatchQueue.main.asyncAfter(deadline: .now() + 3.0) {
-				self.stopScanForBLEDevices()
-			}
-		}
-	
-		func centralManagerDidUpdateState(_ central: CBCentralManager) {
-			print(central.state)
-		}
-	
-		func stopScanForBLEDevices() {
-			centralManager?.stopScan()
-			if((HmSoftPeripheral) != nil){
-				centralManager?.connect(HmSoftPeripheral!, options: nil)
-				//dismiss the progress dialog:
-				alertController.dismiss(animated: true, completion: nil);
-			}
-		}
-	
-		// MARK: - CBCentralManagerDelegate Methods
-		func centralManager(_ central: CBCentralManager, didDiscover peripheral: CBPeripheral, advertisementData: [String : Any], rssi RSSI: NSNumber) {
-			if(peripheral.name == bleShieldName){
-				peripheral.delegate = self;
-				HmSoftPeripheral = peripheral;
-			}
-			print(peripheral)
-		}
-	
+	}
+	*/
 
-		// MARK: BLE Connect delegates
-		func centralManager(_ central: CBCentralManager, didConnect peripheral: CBPeripheral) {
-			peripheral.discoverServices(nil)//all services discovered..may be slow
-			self.collectionView?.isHidden = false //show collection view
-			print("Connected to " +  peripheral.name!)
-		}
-	
-		func centralManager(_ central: CBCentralManager, didFailToConnect peripheral: CBPeripheral, error: Error?) {
-			print(error!)
-		}
-	
-	
-		// MARK: BLE Peripheral delegates
-		func peripheral(_ peripheral: CBPeripheral, didDiscoverServices error: Error?) {
-			guard let services = peripheral.services else { return }
-			for service in services {
-				print(service)
-				peripheral.discoverCharacteristics(nil, for: service)
+
+	// Uncomment this method to specify if the specified item should be selected
+	override func collectionView(_ collectionView: UICollectionView, shouldSelectItemAt indexPath: IndexPath) -> Bool {
+		var sel_index = indexPath.row
+		
+		if(sel_index < 6){
+			if(indexPath.row == 3){
+				sel_index = 2
+			} else if(indexPath.row >= 4){
+				sel_index = 3
 			}
+			let position_string = String(sel_index)
+			let position_string_data = position_string.data(using: String.Encoding.utf8)
+			
+			//Send value to BLE Shield
+			HmSoftPeripheral?.writeValue(position_string_data!, for: self.positionCharacteristic!, type: CBCharacteristicWriteType.withoutResponse)
 		}
+		return true
+	}
+
+	func updateCollectionData(socket_index : Int, status: Int){
+		//Update UI
+		var socket_change = [NSInteger]()
+		
+		if(socket_index == 2){
+			socket_change.append(2)
+			socket_change.append(3)
+		} else if (socket_index == 3){
+			socket_change.append(4)
+			socket_change.append(5)
+		} else {
+			socket_change.append(socket_index)
+		}
+
+		for var item in socket_change {
+			var sel_socket = cv_items[item] as view_socket
+			sel_socket.active = status == 0 ? false : true
+		}
+		
+		self.collectionView?.reloadData()
+	}
+//MARK: BLE Calls
+
+	// MARK: BLE Scanning
+	func scanBLEDevices() {
+		//if you pass nil in the first parameter, then scanForPeriperals will look for any devices.
+		centralManager?.scanForPeripherals(withServices: nil, options: nil)
+		
+		//stop scanning after 3 seconds
+		DispatchQueue.main.asyncAfter(deadline: .now() + 3.0) {
+			self.stopScanForBLEDevices()
+		}
+	}
+
+	func centralManagerDidUpdateState(_ central: CBCentralManager) {
+		print(central.state)
+	}
+
+	func stopScanForBLEDevices() {
+		centralManager?.stopScan()
+		if((HmSoftPeripheral) != nil){
+			centralManager?.connect(HmSoftPeripheral!, options: nil)
+			//dismiss the progress dialog:
+			alertController.dismiss(animated: true, completion: nil);
+		}
+	}
+
+	// MARK: - CBCentralManagerDelegate Methods
+	func centralManager(_ central: CBCentralManager, didDiscover peripheral: CBPeripheral, advertisementData: [String : Any], rssi RSSI: NSNumber) {
+		if(peripheral.name == bleShieldName){
+			peripheral.delegate = self;
+			HmSoftPeripheral = peripheral;
+		}
+		print(peripheral)
+	}
+
+
+	// MARK: BLE Connect delegates
+	func centralManager(_ central: CBCentralManager, didConnect peripheral: CBPeripheral) {
+		peripheral.discoverServices(nil)//all services discovered..may be slow
+		self.collectionView?.isHidden = false //show collection view
+		print("Connected to " +  peripheral.name!)
+	}
+
+	func centralManager(_ central: CBCentralManager, didFailToConnect peripheral: CBPeripheral, error: Error?) {
+		print(error!)
+	}
+
+
+	// MARK: BLE Peripheral delegates
+	func peripheral(_ peripheral: CBPeripheral, didDiscoverServices error: Error?) {
+		guard let services = peripheral.services else { return }
+		for service in services {
+			print(service)
+			peripheral.discoverCharacteristics(nil, for: service)
+		}
+	}
 	
 	func peripheral(_ peripheral: CBPeripheral, didDiscoverCharacteristicsFor service: CBService, error: Error?) {
 		if (peripheral != self.HmSoftPeripheral) {
@@ -208,14 +224,13 @@ class SocketCollectionViewController: UICollectionViewController, CBCentralManag
 		}
 		
 		if (service.uuid.uuidString == BLEService) {
-			
 			for characteristic in service.characteristics! {
-				
 				if (characteristic.uuid.uuidString == BLECharacteristic) {
 					//we'll save the reference, we need it to write data
 					positionCharacteristic = characteristic
-					
 					peripheral.discoverDescriptors(for: characteristic)
+					
+					//peripheral.readValue(for: characteristic)
 					
 					//Set Notify is useful to read incoming data async
 					peripheral.setNotifyValue(true, for: characteristic)
@@ -226,12 +241,15 @@ class SocketCollectionViewController: UICollectionViewController, CBCentralManag
 		}
 	}
 
+	//Reads from Arduino serial monitor
 	func peripheral(_ peripheral: CBPeripheral, didUpdateValueFor characteristic: CBCharacteristic, error: Error?) {
 		if (characteristic.uuid.uuidString == BLECharacteristic) {
 			//data recieved
 			if(characteristic.value != nil) {
-				let stringValue = String(data: characteristic.value!, encoding: String.Encoding.utf8)!
-				print(stringValue)
+				//let stringValue = String(data: characteristic.value!, encoding: String.Encoding.utf8)!
+				self.updateCollectionData(socket_index: Int(characteristic.value![0]), status: Int(characteristic.value![1]))
+				//print(characteristic.value![0])
+				//print(characteristic.value![1])
 			}
 		}
 	}
@@ -242,6 +260,11 @@ class SocketCollectionViewController: UICollectionViewController, CBCentralManag
 	
 	func peripheral(_ peripheral: CBPeripheral, didDiscoverDescriptorsFor characteristic: CBCharacteristic, error: Error?) {
 		print(characteristic.descriptors!)
-	} 
+	}
+	
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
 }
 
